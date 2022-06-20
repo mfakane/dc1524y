403554e27d5194e7a91e4e2c1220762b4d8279be
mfakane <star@sorairo.pictures>
2022-06-21 05:49:17 +0900

    練習人数を考慮したマクロを生成する
    対象が存在しないとき wait 指定が飛ばされてしまうため、該当者がいないときはあらかじめ wait コマンドに置き換える

--- src/thordan2/woth.tsx
+++ src/thordan2/woth.tsx
@@ -1,25 +1,27 @@
 import { FunctionComponent } from "preact";
 import { useState } from "preact/hooks";
 import RandomBag from "@/randomBag.js";
-import Marker from "@/marker.js";
 
 type PrimaryTargetKind =
   | "skywardLeap"
-  | "spiralPierce"
+  | "spiralPierce1"
+  | "spiralPierce2"
   | "cauterize"
   | undefined;
 type SecondaryTargetKind =
   | "altarFlare"
   | "liquidHeaven"
-  | "thunderStruck"
+  | "thunderStruck1"
+  | "thunderStruck2"
   | undefined;
 type TargetKind = PrimaryTargetKind | SecondaryTargetKind;
 
-const generate = (prefix: string, continuation: string) => {
+const generate = (playerCount: number, continuation: string) => {
+  const prefix = playerCount === 1 ? "/e " : "/p ";
   const primaryBag = new RandomBag<PrimaryTargetKind>([
     "skywardLeap",
-    "spiralPierce",
-    "spiralPierce",
+    "spiralPierce1",
+    "spiralPierce2",
     "cauterize",
     undefined,
     undefined,
@@ -28,7 +30,8 @@ const generate = (prefix: string, continuation: string) => {
   ]);
   const players = [...Array(8)].map((_, i) => {
     return {
-      noun: `<${i + 1}>`,
+      index: i,
+      pronoun: `<${i + 1}>`,
       primaryTarget: primaryBag.pop(),
       secondaryTarget: undefined as SecondaryTargetKind,
     };
@@ -41,85 +44,78 @@ const generate = (prefix: string, continuation: string) => {
   RandomBag.selectMany(
     2,
     players.filter((x) => !x.secondaryTarget)
-  ).forEach((x) => (x.secondaryTarget = "thunderStruck"));
+  ).forEach(
+    (x, i) =>
+      (x.secondaryTarget = i === 0 ? "thunderStruck1" : "thunderStruck2")
+  );
 
-  const markerMap: [TargetKind, Marker][] = [
-    ["skywardLeap", "circle"],
-    ["spiralPierce", "bind1"],
-    ["spiralPierce", "bind2"],
-    ["cauterize", "triangle"],
-    ["liquidHeaven", "attack4"],
-    ["altarFlare", "attack3"],
-    ["thunderStruck", "stop1"],
-    ["thunderStruck", "stop2"],
-  ];
-  const getMarker = (kind: TargetKind): Marker => {
-    const index = markerMap.findIndex((x) => x[0] === kind);
-    if (index === -1) return "off";
-
-    const marker = markerMap[index][1];
-    markerMap.splice(index, 1);
-    return marker;
+  const playerTarget = (
+    kind: TargetKind,
+    text: (pronoun: string) => string,
+    whenNotPresent?: string
+  ) => {
+    const player = players.find(
+      (x) => x.primaryTarget === kind || x.secondaryTarget === kind
+    );
+    return player && player.index < playerCount
+      ? text(player.pronoun)
+      : whenNotPresent ?? "";
   };
-  const playersByMarker = new Map<Marker, string>();
-  for (const player of players) {
-    if (player.primaryTarget)
-      playersByMarker.set(getMarker(player.primaryTarget), player.noun);
-    if (player.secondaryTarget)
-      playersByMarker.set(getMarker(player.secondaryTarget), player.noun);
-  }
 
   return [
-    `
-${prefix}騎神トールダンは「至天の陣：風槍」の構え。<se.3> <wait.4>
+    `${prefix}騎神トールダンは「至天の陣：風槍」の構え。<se.3> <wait.4>
 ${prefix}騎神トールダンの「至天の陣：風槍」 <se.3> <wait.6>
-/mk circle ${playersByMarker.get("circle")}
-/mk bind1 ${playersByMarker.get("bind1")}
-/mk bind2 ${playersByMarker.get("bind2")} <wait.5>
-/mk stop1 ${playersByMarker.get("stop1")}
-/mk stop2 ${playersByMarker.get("stop2")} <wait.2>
+${playerTarget("skywardLeap", (x) => `/mk circle ${x}`)}
+${playerTarget("spiralPierce1", (x) => `/mk bind1 ${x}`)}
+${playerTarget("spiralPierce2", (x) => `/mk bind2 ${x} <wait.5>`, "/wait 5")}
+${playerTarget("thunderStruck1", (x) => `/mk stop1 ${x}`)}
+${playerTarget("thunderStruck1", (x) => `/mk stop2 ${x} <wait.2>`, "/wait 2")}
 /mk off <circle>
 /mk off <bind1>
 /mk off <bind2>
-/mk triangle ${playersByMarker.get("triangle")}
+${playerTarget("cauterize", (x) => `/mk triangle ${x}`)}
 ${prefix}ヴェズルフェルニルの「ツイスターダイブ」 <se.5> <wait.6>
 /mk off <triangle>
 ${prefix}騎神トールダンの「レベレーション・アスカロンメルシー」 <se.4>
-${continuation}
-`.trim(),
-    `
-/mk attack3 ${playersByMarker.get("attack3")}
-/mk attack4 ${playersByMarker.get("attack4")}
+${continuation}`,
+    `${playerTarget("liquidHeaven", (x) => `/mk attack4 ${x}`)}
+${playerTarget("altarFlare", (x) => `/mk attack3 ${x}`)}
 ${prefix}ヘヴンリキッド (1), アルターフレア (1) <se.3> <wait.1>
-/mk attack2 <attack3>
-/mk attack3 <attack4>
+${playerTarget("liquidHeaven", (x) => `/mk attack3 ${x}`)}
+${playerTarget("altarFlare", (x) => `/mk attack2 ${x}`)}
 ${prefix}ヘヴンリキッド (2), アルターフレア (2) <se.3> <wait.1>
-/mk attack1 <attack2>
-/mk attack2 <attack3>
+${playerTarget("liquidHeaven", (x) => `/mk attack2 ${x}`)}
+${playerTarget("altarFlare", (x) => `/mk attack1 ${x}`)}
 ${prefix}ヘヴンリキッド (3), アルターフレア (3) <se.3> <wait.1>
-/mk off <attack1>
-/mk attack1 <attack2>
+${playerTarget("liquidHeaven", (x) => `/mk attack1 ${x}`)}
+${playerTarget("altarFlare", (x) => `/mk off ${x}`)}
 ${prefix}ヘヴンリキッド (4), アルターフレア (4) <se.3> <wait.1>
-/mk off <attack1>
+${playerTarget("liquidHeaven", (x) => `/mk off ${x}`)}
 ${prefix}ヘヴンリキッド (5) <se.3> <wait.1>
-${prefix}聖騎士グリノーの「エンプティディメンション」 <se.4>`.trim(),
+${prefix}聖騎士グリノーの「エンプティディメンション」 <se.4>`,
   ];
 };
 
 const WrathOfTheHeavens: FunctionComponent = () => {
-  const [prefix, setPrefix] = useState("/p ");
+  const [playerCount, setPlayerCount] = useState(8);
   const [continuation, setContinuation] = useState("/e 次のマクロへ！ <se.7>");
-  const [text, setText] = useState(generate(prefix, continuation));
+  const [text, setText] = useState(generate(playerCount, continuation));
   return (
     <form onSubmit={(e) => e.preventDefault()}>
       <p>
-        チャットコマンド:{" "}
+        練習人数:{" "}
         <select
-          value={prefix}
-          onChange={(e) => setPrefix(e.currentTarget.value)}
+          value={playerCount}
+          onChange={(e) => setPlayerCount(parseInt(e.currentTarget.value))}
         >
-          <option value="/p ">/p </option>
-          <option value="/e ">/e </option>
+          <option value={8}>8</option>
+          <option value={7}>7</option>
+          <option value={6}>6</option>
+          <option value={5}>5</option>
+          <option value={4}>4</option>
+          <option value={3}>3</option>
+          <option value={2}>2</option>
+          <option value={1}>1</option>
         </select>
         <br />
         マクロ継続通知:{" "}
@@ -129,17 +125,18 @@ const WrathOfTheHeavens: FunctionComponent = () => {
           onChange={(e) => setContinuation(e.currentTarget.value)}
         />
         <br />
-        <button onClick={() => setText(generate(prefix, continuation))}>
+        <button onClick={() => setText(generate(playerCount, continuation))}>
           生成
         </button>
       </p>
-      <textarea readonly rows={15} cols={80}>
-        {text[0]}
-      </textarea>
-      <br />
-      <textarea readonly rows={15} cols={80}>
-        {text[1]}
-      </textarea>
+      {text.map((x) => (
+        <>
+          <textarea readonly rows={15} cols={80}>
+            {x}
+          </textarea>
+          <br />
+        </>
+      ))}
     </form>
   );
 };
